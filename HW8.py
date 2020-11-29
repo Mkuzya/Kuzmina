91cdf84f6f45aaa3d83793bad7682711d1790a15
Mkuzya <59773134+Mkuzya@users.noreply.github.com>
2020-11-29 13:10:44 +0300

    HW8

---
+++ HW8.py
@@ -0,0 +1,170 @@
+class Data:
+    def __init__(self, day_month_year):
+        self.day_month_year = str(day_month_year)
+
+    @classmethod
+    def extract(cls, day_month_year):
+        my_date = []
+
+        for i in day_month_year.split():
+            if i != '-': my_date.append(i)
+
+        return int(my_date[0]), int(my_date[1]), int(my_date[2])
+
+    @staticmethod
+    def valid(day, month, year):
+
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 2019 >= year >= 0:
+                    return f'Верно'
+                else:
+                    return f'Неправильный год'
+            else:
+                return f'Неправильный месяц'
+        else:
+            return f'Неправильный день'
+
+    def __str__(self):
+        return f'Текущая дата {Data.extract(self.day_month_year)}'
+
+
+today = Data('29 - 11 - 2020')
+print(today)
+print(Data.valid(1, 11, 2018))
+print(today.valid(11, 9, 2019))
+print(Data.extract('28 - 11 - 2021'))
+print(today.extract('11 - 11 - 2020'))
+print(Data.valid(1, 11, 2000))
+
+
+class DivisionByNull:
+    def __init__(self, divider, denominator):
+        self.divider = divider
+        self.denominator = denominator
+
+    @staticmethod
+    def divide_by_null(divider, denominator):
+        try:
+            return (divider / denominator)
+        except:
+            return (f"Нельзя делить на 0")
+
+
+div = DivisionByNull(10, 100)
+print(DivisionByNull.divide_by_null(10, 0))
+print(DivisionByNull.divide_by_null(10, 0.1))
+print(div.divide_by_null(100, 0))
+
+
+class Error:
+    def __init__(self, *args):
+        self.my_list = []
+
+    def my_input(self):
+
+        while True:
+            try:
+                val = int(input('Введите значения и нажимите Enter - '))
+                self.my_list.append(val)
+                print(f'Текущий список - {self.my_list} \n ')
+            except:
+                print(f"Недопустимое значение.")
+                y_or_n = input(f'Попробовать еще раз? Да/Нет ')
+
+                if y_or_n == 'Y' or y_or_n == 'y':
+                    print(try_except.my_input())
+                elif y_or_n == 'N' or y_or_n == 'n':
+                    return f'Вы вышли'
+                else:
+                    return f'Вы вышли'
+
+try_except = Error(1)
+print(try_except.my_input())
+
+
+
+class StoreMashines:
+
+    def __init__(self, name, price, quantity, number_of_lists, *args):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.numb = number_of_lists
+        self.my_store_full = []
+        self.my_store = []
+        self.my_unit = {'Модель устройства': self.name, 'Цена за единицу': self.price, 'Количество': self.quantity}
+
+    def __str__(self):
+        return f'{self.name} цена {self.price} количество {self.quantity}'
+
+    def reception(self):
+        try:
+            unit = input(f'Введите наименование: ')
+            unit_p = int(input(f'Введите цену за единицу: '))
+            unit_q = int(input(f'Введите количество: '))
+            unique = {'Модель устройства': unit, 'Цена за единицу': unit_p, 'Количество': unit_q}
+            self.my_unit.update(unique)
+            self.my_store.append(self.my_unit)
+            print(f'Текущий список -\n {self.my_store}')
+        except:
+            return f'Ошибка ввода данных'
+
+        print(f'Для выхода - Q, продолжение - Enter')
+        q = input(f'---> ')
+        if q == 'Q' or q == 'q':
+            self.my_store_full.append(self.my_store)
+            print(f'Весь склад -\n {self.my_store_full}')
+            return f'Выход'
+        else:
+            return StoreMashines.reception(self)
+
+class Printer(StoreMashines):
+    def to_print(self):
+        return f' {self.numb} '
+
+
+class Scanner(StoreMashines):
+    def to_scan(self):
+        return f' {self.numb} '
+
+
+class Copier(StoreMashines):
+    def to_copier(self):
+        return f' {self.numb} '
+
+
+unit_1 = Printer('hp', 10000, 4, 10)
+unit_2 = Scanner('Canon', 12000, 50, 18)
+unit_3 = Copier('Xerox', 15000, 10, 15)
+print(unit_1.reception())
+print(unit_2.reception())
+print(unit_3.reception())
+print(unit_1.to_print())
+print(unit_3.to_copier())
+
+
+
+class ComplexNumber:
+    def __init__(self, a, b, *args):
+        self.a = a
+        self.b = b
+        self.c = 'a + b * i'
+
+    def __add__(self, other):
+        print(f'Сумма c1 и c2 равна')
+        return f'c = {self.a + other.a} + {self.b + other.b} * i'
+
+    def __mul__(self, other):
+        print(f'Произведение c1 и c2 равно')
+        return f'c = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+
+    def __str__(self):
+        return f'c = {self.a} + {self.b} * i'
+
+
+c_1 = ComplexNumber(5, 8)
+c_2 = ComplexNumber(10, 4)
+print(c_1)
+print(c_1 + c_2)
+print(c_1 * c_2)
